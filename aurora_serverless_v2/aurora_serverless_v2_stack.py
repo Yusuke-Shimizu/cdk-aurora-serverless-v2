00ae6c112ffce3d018017502b02283bab211a064
EC2 Default User <ec2-user@ip-172-31-21-92.ap-northeast-1.compute.internal>
2022-08-30 21:14:20 +0000

    mod

--- aurora_serverless_v2/aurora_serverless_v2_stack.py
+++ aurora_serverless_v2/aurora_serverless_v2_stack.py
@@ -4,11 +4,14 @@ from aws_cdk import (
     Environment,
     Tags,
     Duration,
+    CustomResource,
     custom_resources as cr,
     aws_ec2 as ec2,
     aws_rds as rds,
     aws_secretsmanager as secretsmanager,
     aws_kms as kms,
+    aws_iam as iam,
+    aws_lambda as lambda_,
 )
 from constructs import Construct
 
@@ -27,80 +30,109 @@ class AuroraServerlessV2Stack(Stack):
         super().__init__(scope, _id, **kwargs)
         
         vpc = ec2.Vpc(self, "VPC")
-
+        
+        db_user = "admin"
         secret = rds.DatabaseSecret(self, "AuroraSecret", username=db_user)
-
         aurora_cluster_credentials = rds.Credentials.from_secret(secret, db_user)
 
-        kms_key = kms.Key(
-            self, "AuroraDatabaseKey", enable_key_rotation=True, alias=_id
+        kms_key = kms.Key(self, "AuroraDatabaseKey", 
+            # self, "AuroraDatabaseKey", enable_key_rotation=True, alias=_id
+            enable_key_rotation=True
         )
-
-        # parameter_group = rds.ParameterGroup.from_parameter_group_name(
-        #     self, "ParameterGroup", "default.aurora-postgresql14"
-        # )
 
         instance_count = 1
 
         cluster = rds.DatabaseCluster(self, "AuroraCluster",
+            cluster_identifier='y3-shimizu-cdk-aurora',
             engine=rds.DatabaseClusterEngine.aurora_mysql(version=rds.AuroraMysqlEngineVersion.VER_3_01_0),
-            # cluster_identifier=_id,
             instances=instance_count,
             instance_props=rds.InstanceProps(
                 vpc=vpc,
                 # instance_type=ec2.InstanceType("serverless"),
-                # auto_minor_version_upgrade=True,
-                # allow_major_version_upgrade=False,
-                # publicly_accessible=True,
             ),
-            # engine=rds.DatabaseClusterEngine.AURORA_POSTGRESQL,
-            # engine_version="14.3",
-            # parameter_group=parameter_group,
             credentials=aurora_cluster_credentials,
-            # default_database_name=db_name,
-            # backup=rds.BackupProps(
-            #     retention=Duration.days(14),
-            # ),
+        )
+        
+        mod_cluster_serverless = cr.AwsSdkCall(
+            service="RDS",
+            action="modifyDBCluster",
+            parameters={
+                "DBClusterIdentifier": cluster.cluster_identifier,
+                "ApplyImmediately": True,
+                "EngineVersion": "8.0.mysql_aurora.3.02.0",
+                # "ServerlessV2ScalingConfiguration": {
+                #     "MinCapacity": 0.5,
+                #     "MaxCapacity": 1,
+                # },
+            },
+            physical_resource_id=cr.PhysicalResourceId.of(
+                cluster.cluster_identifier
+            ),
+        )
+        mod_cluster_provisioned = cr.AwsSdkCall(
+            service="RDS",
+            action="modifyDBCluster",
+            parameters={
+                "DBClusterIdentifier": cluster.cluster_identifier,
+                "ApplyImmediately": True,
+                "EngineVersion": "8.0.mysql_aurora.3.01.0",
+            },
+            physical_resource_id=cr.PhysicalResourceId.of(
+                cluster.cluster_identifier
+            ),
+        )
+        version_up = cr.AwsCustomResource(self, "VersionUp",
+            on_create=mod_cluster_serverless,
+            on_update=mod_cluster_serverless,
+            # on_delete=mod_cluster_provisioned,
+            policy=cr.AwsCustomResourcePolicy.from_sdk_calls(
+                resources=cr.AwsCustomResourcePolicy.ANY_RESOURCE,
+            ),
         )
 
-        # parameters = {
-        #     "DBClusterIdentifier": cluster.cluster_identifier,
-        #     "ServerlessV2ScalingConfiguration": {
-        #         "MinCapacity": 0.5,
-        #         "MaxCapacity": 8,
-        #     },
-        # }
+        mod_type_serverless = cr.AwsSdkCall(
+            service="RDS",
+            action="modifyDBInstance",
+            parameters={
+                "DBInstanceIdentifier": cluster.instance_identifiers[0],
+                "DBInstanceClass": "db.serverless",
+                "ApplyImmediately": True
+            },
+            physical_resource_id=cr.PhysicalResourceId.of(
+                cluster.instance_identifiers[0]
+            ),
+        )
+        mod_type_provisioned = cr.AwsSdkCall(
+            service="RDS",
+            action="modifyDBInstance",
+            parameters={
+                "DBInstanceIdentifier": cluster.instance_identifiers[0],
+                "DBInstanceClass": "db.t3.medium",
+                "ApplyImmediately": True
+            },
+            physical_resource_id=cr.PhysicalResourceId.of(
+                cluster.instance_identifiers[0]
+            ),
+        )
+        modify_instance_type = cr.AwsCustomResource(self, "ModType",
+            on_create=mod_type_serverless,
+            on_update=mod_type_serverless,
+            on_delete=mod_type_provisioned,
+            policy=cr.AwsCustomResourcePolicy.from_sdk_calls(
+                resources=cr.AwsCustomResourcePolicy.ANY_RESOURCE,
+            ),
+        )
 
-        # scaling = cr.AwsCustomResource(
-        #     self,
-        #     "Scaling",
-        #     on_create=cr.AwsSdkCall(
-        #         service="RDS",
-        #         action="modifyDBCluster",
-        #         parameters=parameters,
-        #         physical_resource_id=cr.PhysicalResourceId.of(
-        #             cluster.cluster_identifier
-        #         ),
-        #     ),
-        #     on_update=cr.AwsSdkCall(
-        #         service="RDS",
-        #         action="modifyDBCluster",
-        #         parameters=parameters,
-        #         physical_resource_id=cr.PhysicalResourceId.of(
-        #             cluster.cluster_identifier
-        #         ),
-        #     ),
-        #     policy=cr.AwsCustomResourcePolicy.from_sdk_calls(
-        #         resources=cr.AwsCustomResourcePolicy.ANY_RESOURCE,
-        #     ),
-        # )
+        cfnCluster = cluster.node.default_child
 
-        # cfnCluster = cluster.node.default_child
-
-        # target = scaling.node.find_child("Resource").node.default_child
+        target = version_up.node.find_child("Resource").node.default_child
+        target_mod_type = modify_instance_type.node.default_child
 
         # cfnCluster.add_property_override("EngineMode", "provisioned")
-        # scaling.node.add_dependency(cfnCluster)
+        version_up.node.add_dependency(cfnCluster)
+        # version_up.node.add_dependency(cluster)
+        modify_instance_type.node.add_dependency(target)
 
-        # for i in range(1, instance_count + 1):
-        #     cluster.node.find_child(f"Instance{i}").add_depends_on(target)
+        for i in range(1, instance_count + 1):
+            # cluster.node.find_child(f"Instance{i}").add_depends_on(target)
+            modify_instance_type.node.add_dependency(cluster.node.find_child(f"Instance{i}"))
---
+++ scripts/mod_aurora.py
@@ -0,0 +1,54 @@
+import boto3
+import pprint
+import datetime
+import time
+
+client = boto3.client('rds')
+instance_status = True
+# db_cluster_name = 'y3-shimizu-senju-db'
+db_cluster_name = 'y3-shimizu-cdk-aurora'
+db_instance_name = db_cluster_name + 'instance1'
+
+client_lambda = boto3.client('lambda')
+function_name = "y3-shimizu_cluster_verup"
+
+print('--- Start ---')
+while instance_status:
+    now = datetime.datetime.now()
+    print(now)
+    
+    response = client.describe_db_clusters(
+        DBClusterIdentifier=db_cluster_name,
+    )
+    cluster_name = response['DBClusters'][0]['DBClusterIdentifier']
+    cluster_status = response['DBClusters'][0]['Status']
+    cluster_engine = response['DBClusters'][0]['EngineVersion']
+    pprint.pprint(cluster_name + ' : ' + cluster_status + ', ' + cluster_engine)
+    
+    try:
+        response = client.describe_db_instances(
+            DBInstanceIdentifier=db_instance_name,
+        )
+        db_name = response['DBInstances'][0]['DBInstanceIdentifier']
+        db_status = response['DBInstances'][0]['DBInstanceStatus']
+        db_class = response['DBInstances'][0]['DBInstanceClass']
+        pprint.pprint(db_name + ' : ' + db_status + ', ' + db_class)
+        if db_status == 'available':
+            instance_status = False
+    except:
+        print('no instance')
+    
+
+    try:
+        response = client.get_function(
+            FunctionName=function_name,
+        )
+        pprint.pprint(response)
+    except:
+        print('no function')
+
+
+    print('---')
+    time.sleep(10)
+
+print('--- End ---')
